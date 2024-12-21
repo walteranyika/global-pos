544f62b4cea5fdb89c4447b83fd4aed506466f64
walter <walter@tradesy.com>
2024-12-21 17:39:33 +0300

    Fix reports summary

--- app/Services/DailyReportService.php
+++ app/Services/DailyReportService.php
@@ -14,12 +14,14 @@ use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 use Mike42\Escpos\Printer;
 
-class DailyReportService{
-    public function getData(){
+class DailyReportService
+{
+    public function getData()
+    {
         Item::truncate();
         $sales = Sale::with('details', 'facture')
             ->where('deleted_at', '=', null)
-            ->whereDate("created_at",Carbon::today())
+            ->whereDate("created_at", Carbon::today())
             ->get();
         $group_id = rand(1000, 100000);
         $products = Product::where('deleted_at', '=', null)->get();
@@ -48,7 +50,7 @@ class DailyReportService{
             $item->save();
             $total_sales += $item->price;
         }
-        $tasks =  Item::where(['group_id' => $group_id])->orderBy('price', 'desc')->get();
+        $tasks = Item::where(['group_id' => $group_id])->orderBy('price', 'desc')->get();
         $data = [];
         $total_overall = 0;
         foreach ($tasks as $item) {
@@ -60,11 +62,12 @@ class DailyReportService{
                 $data[] = $row;
             }
         }
-        return  $data;
+        return $data;
     }
 
-    public function getDailyReport(){
-        $today= Carbon::today()->subDays(10)->format('Y-m-d');
+    public function getDailyReport()
+    {
+        $today = Carbon::today()->subDays(10)->format('Y-m-d');
         $query = "SELECT p.name AS Product, p.price AS Price,subquery.* FROM (SELECT sd.product_id as product_id,
                         SUM(sd.total) as Total,
                         SUM(sd.quantity) as Quantity FROM sale_details sd
@@ -76,7 +79,7 @@ class DailyReportService{
                     ON subquery.product_id=p.id
                     ORDER BY subquery.Total DESC";
 
-        $data= DB::select($query, [$today]);
+        $data = DB::select($query, [$today]);
         $summary_query = "SELECT SUM(ps.montant) as Total, ps.Reglement as Method FROM payment_sales ps
                           WHERE ps.sale_id IN (SELECT id FROM sales WHERE DATE(created_at) = CURDATE() AND deleted_at is NULL)
                           GROUP BY ps.Reglement";
@@ -84,15 +87,28 @@ class DailyReportService{
         $unpaid_partial_credit = "SELECT SUM(s.GrandTotal - s.paid_amount) as Total , s.statut as Method FROM sales s
                                   WHERE DATE(s.created_at) = CURDATE()
                                   AND s.deleted_at is NULL
-                                  AND s.statut NOT IN ('completed')
+                                  AND s.payment_statut IN ('unpaid','partial')
                                   GROUP BY s.statut";
 
         $unpaid_summary = DB::select($unpaid_partial_credit);
         $summary = DB::select($summary_query);
-        return  ['data'=>$data, 'summary'=>array_merge($summary, $unpaid_summary)];
+        $all_summaries = array_merge($summary, $unpaid_summary);
+
+        $t1=0;
+        for ($i = 0; $i < sizeof($data); $i++) {
+            $t1 = $t1 + $data[$i]->Total;
+        }
+        $t2 =0;
+        for ($j = 0; $j < sizeof($all_summaries); $j++) {
+            $t2 = $t2 + $all_summaries[$j]->Total;
+        }
+        Log::info("Total Main ", [$t1]);
+        Log::info("Total Payments ", [$t2]);
+        return ['data' => $data, 'summary' => $all_summaries];
     }
 
-    public function getMonthyReport($from, $to){
+    public function getMonthyReport($from, $to)
+    {
         $from = $from->format("Y-m-d");
         $to = $to->format("Y-m-d");
         $query = "SELECT p.name AS Product, p.price AS Price,subquery.* FROM (SELECT sd.product_id as product_id,
@@ -108,7 +124,7 @@ class DailyReportService{
                     ON subquery.product_id=p.id
                     ORDER BY subquery.Total DESC";
 
-        $data= DB::select($query, [$from, $to]);
+        $data = DB::select($query, [$from, $to]);
 
         $summary_query = "SELECT SUM(ps.montant) as Total, ps.Reglement as Method FROM payment_sales ps
                             WHERE ps.sale_id IN (SELECT id FROM sales WHERE DATE(created_at) >= ? AND DATE(created_at) <= ? AND deleted_at is NULL)
@@ -119,52 +135,54 @@ class DailyReportService{
         $unpaid_partial_credit = "SELECT SUM(s.GrandTotal - s.paid_amount) as Total , s.statut as Method FROM sales s
                                   WHERE DATE(s.created_at) >= ? AND DATE(s.created_at) <= ?
                                   AND s.deleted_at is NULL
-                                  AND s.statut NOT IN ('completed')
+                                  AND s.payment_statut IN ('unpaid','partial')
                                   GROUP BY s.statut";
+        $unpaid_summary = DB::select($unpaid_partial_credit, [$from, $to]);
+        $all_summaries = array_merge($summary, $unpaid_summary);
 
-        $unpaid_summary = DB::select($unpaid_partial_credit,[$from, $to]);
-        return  ['data'=>$data, 'summary'=>array_merge($summary, $unpaid_summary)];
+        return ['data' => $data, 'summary' => $all_summaries];
     }
 
-    public function getPrintConnector(){
-        $connector = null;
-        $os= strtolower(php_uname('s'));
-        try{
-            if($os=='linux'){
-                $subject=shell_exec("ls /dev/usb/ | grep lp");
+    public function getPrintConnector()
+    {
+        $os = strtolower(php_uname('s'));
+        try {
+            if ($os == 'linux') {
+                $subject = shell_exec("ls /dev/usb/ | grep lp");
                 preg_match_all('/(lp\d)/', $subject, $match);
-                if(!empty($subject) && !empty($match)){
-                    $device_url = "/dev/usb/".$match[0][0];
-                }else{
-                    $device_url="/dev/usb/lp0";
+                if (!empty($subject) && !empty($match)) {
+                    $device_url = "/dev/usb/" . $match[0][0];
+                } else {
+                    $device_url = "/dev/usb/lp0";
                 }
                 Log::info($device_url);
                 //$connector = new FilePrintConnector($device_url);
                 //$connector = new FilePrintConnector("/dev/usb/lp0");
                 $connector = new FilePrintConnector("php://stdout");
-               // $connector = new FilePrintConnector("data.txt");
-            }else if($os=="windows nt"){
+                // $connector = new FilePrintConnector("data.txt");
+            } else if ($os == "windows nt") {
                 $connector = new WindowsPrintConnector("pos_print");
-            }else{
+            } else {
                 $connector = new FilePrintConnector("php://stdout");
             }
             //$connector = new FilePrintConnector("data.txt");
             //$connector = new FilePrintConnector("/dev/usb/lp1");
             //$connector = new NetworkPrintConnector("10.x.x.x", 9100);
 
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             $connector = new FilePrintConnector("php://stdout");
-            Log::error("Could not get the printer connector. ". $e->getMessage());
+            Log::error("Could not get the printer connector. " . $e->getMessage());
         }
         return $connector;
     }
 
 
-    public function print(){
+    public function print()
+    {
 
         $details = $this->getDailyReport();
         $connector = $this->getPrintConnector();
-        if($connector==null){
+        if ($connector == null) {
             Log::info("NUll printer");
             return;
         }
@@ -172,8 +190,8 @@ class DailyReportService{
         $printer = new Printer($connector);
         $printer->setJustification(Printer::JUSTIFY_CENTER);
         $printer->selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
-        $printer -> setFont(Printer::FONT_B);
-        $printer -> setTextSize(2, 2);
+        $printer->setFont(Printer::FONT_B);
+        $printer->setTextSize(2, 2);
         $printer->text("NICEFRIES GRILL & LOUNGE\n");
         $printer->selectPrintMode();
         $printer->setEmphasis(true);
@@ -187,8 +205,8 @@ class DailyReportService{
         $printer->feed();
         $date = Carbon::now();
 
-        $printer->text("Sales Report For ".$date->format('d/m/Y')."\n");
-        $printer->text("Generated At ".$date->format('H:i A')."\n");
+        $printer->text("Sales Report For " . $date->format('d/m/Y') . "\n");
+        $printer->text("Generated At " . $date->format('H:i A') . "\n");
         $printer->feed(2);
 
         $printer->setJustification(Printer::JUSTIFY_LEFT);
@@ -202,7 +220,7 @@ class DailyReportService{
         $printer->setEmphasis(true);
 
         //title of the receipt
-       // $printer->text("Order For $client->name\n");
+        // $printer->text("Order For $client->name\n");
 
         $printer->setJustification(Printer::JUSTIFY_LEFT);
         $printer->setEmphasis(false);
@@ -213,26 +231,25 @@ class DailyReportService{
         $printer->text(str_repeat(".", 48) . "\n");
         //Print product details
         $total = 0;
-        $extras =[];
+        $extras = [];
         foreach ($details as $key => $value) {
-           if ($value['Product'] !=""){
-            $product = new PrintableItem($value['Product'], $value['Total']/$value['Quantity'],  $value['Quantity']);
-            $printer->text($product->getPrintatbleRowMod());
-            $total += $value['Total'];
-           }else{
-             $extras[] = ["name"=>$value['Quantity'], "total"=>$value['Total']];
-           }
+            if ($value['Product'] != "") {
+                $product = new PrintableItem($value['Product'], $value['Total'] / $value['Quantity'], $value['Quantity']);
+                $printer->text($product->getPrintatbleRowMod());
+                $total += $value['Total'];
+            } else {
+                $extras[] = ["name" => $value['Quantity'], "total" => $value['Total']];
+            }
         }
         $printer->text(str_repeat(".", 48) . "\n");
         $printer->selectPrintMode();
 
 
-        foreach($extras as $key => $value)
-         {
+        foreach ($extras as $key => $value) {
             // Log::info("Method ".$value["name"]);
             $sub_total_text = str_pad($value["name"], 36, ' ') . str_pad(number_format($value["total"]), 12, ' ', STR_PAD_LEFT);
-            $printer->text(strtoupper($sub_total_text)."\n");
-         }
+            $printer->text(strtoupper($sub_total_text) . "\n");
+        }
 
         // $grand_total_text = str_pad("GRAND TOTAL", 36, ' ') . str_pad(number_format($total), 12, ' ', STR_PAD_LEFT);
         // $printer->text($grand_total_text);
@@ -253,9 +270,7 @@ class DailyReportService{
 
         $printer->cut();
         $printer->close();
-       }
-
-
+    }
 
 
 }
