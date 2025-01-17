2fcb410ac9ab6863455242cc5ce6950ef7cb589d
walter <walter@tradesy.com>
2025-01-17 08:14:53 +0300

    Fix excel report

--- app/Exports/DailySalesExport.php
+++ app/Exports/DailySalesExport.php
@@ -2,25 +2,14 @@
 
 namespace App\Exports;
 
-use App\Models\Item;
-use App\Models\Product;
-use App\Models\product_warehouse;
-use App\Models\Role;
-use App\Models\Sale;
-use App\Models\PaymentSale;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\RegistersEventListeners;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Events\AfterSheet;
-use DB;
-use Illuminate\Support\Facades\Log;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-use Maatwebsite\Excel\Concerns\WithStyles;
-use PhpOffice\PhpSpreadsheet\Style\Font;
-use Maatwebsite\Excel\Concerns\RegistersEventListeners;
 
 
 class DailySalesExport implements FromArray, WithHeadings, ShouldAutoSize, WithEvents
@@ -43,131 +32,32 @@ class DailySalesExport implements FromArray, WithHeadings, ShouldAutoSize, WithE
 
     public function array(): array
     {
-        Item::truncate();
-        if ($this->fromDate != "") {
-            $from =  Carbon::createFromFormat('Y-m-d H:i a', $this->fromDate, 'Africa/Nairobi');
-        } else {
-            $from = Carbon::now()->subDay();
-        }
+        $from =  Carbon::createFromFormat('Y-m-d H:i a', $this->fromDate, 'Africa/Nairobi');
+        $to =  Carbon::createFromFormat('Y-m-d H:i a', $this->toDate, 'Africa/Nairobi');
 
-        if ($this->toDate != "") {
-            $to =  Carbon::createFromFormat('Y-m-d H:i a', $this->toDate, 'Africa/Nairobi');
-        } else {
-            $to = Carbon::now();
-        }
-        $sales = Sale::with('details', 'facture')
-            ->where('deleted_at', '=', null)
-            ->where("created_at", ">=", $from)
-            ->where("created_at", "<=", $to)
-            ->get();
-        $group_id = rand(1000, 100000);
-        $products = Product::where('deleted_at', '=', null)->get();
-        foreach ($products as $product) {
-            $product_closing_stock = product_warehouse::where('product_id', $product->id)
-                ->where('deleted_at', '=', null)
-                ->get();
-            $closing_stock = 0;
-            foreach ($product_closing_stock as $product_warehouse) {
-                $closing_stock += $product_warehouse->qte;
-            }
-            Item::create(["product_id" => $product->id, "product_name" => $product->name, 'closing_stock' => $closing_stock, "group_id" => $group_id]);
-        }
-        foreach ($sales as $sale) {
-            $details = $sale->details;
-            foreach ($details as $detail) {
-                $product_id = $detail->product_id;
-                $item = Item::where(["product_id" => $product_id, "group_id" => $group_id])->first();
-                $item->sales += $detail->quantity;
-                $item->price += $detail->total;
-                $item->save();
-            }
-        }
+        $query = "SELECT p.name AS product, p.shop,  p.price AS price, subquery.Quantity, subquery.Total FROM (SELECT sd.product_id as product_id,
+                        SUM(sd.total) as Total,
+                        SUM(sd.quantity) as Quantity FROM sale_details sd
+                        JOIN sales s
+                         ON sd.sale_id=s.id
+                            WHERE s.created_at >=?
+                            AND s.created_at <=?
+                            AND s.deleted_at is NULL
+                    GROUP BY sd.product_id) as subquery
+                    JOIN products p
+                    ON subquery.product_id=p.id
+                    ORDER BY p.shop, subquery.Total DESC";
 
-        $items = Item::where(['group_id' => $group_id])->get();
-        $total_sales = 0;
-        foreach ($items as $item) {
-            $item->opening_stock = $item->closing_stock + $item->sales;
-            $item->save();
-            $total_sales += $item->price;
-        }
-        $tasks =  Item::where(['group_id' => $group_id])->orderBy('price', 'desc')->get();
-        $data = [];
-        $i = 1;
-        $total_overall = 0;
-        foreach ($tasks as $item) {
-            if ($item->sales > 0) {
-                $row['#'] = $i;
-                $row['Product'] = $item->product_name;
-                $row['Quantity'] = (empty($item->sales)) ? '0' : $item->sales;
-                $row['Total'] = (empty($item->price)) ? '0' : $item->price;
-                $total_overall += $item->price;
-                $i++;
-                $data[] = $row;
-            }
-        }
-
-        $grouped = PaymentSale::where('deleted_at', '=', null)
-            ->where("created_at", ">=", $from)
-            ->where("created_at", "<=", $to)
-            ->select(DB::raw('SUM(montant) As sum, Reglement'))
-            ->groupBy('Reglement')
-            ->get();
-
-        $total = array("#" => "", "Product" => "", "Quantity" => "Total", "Total" => $total_overall);
-        foreach ($grouped as $item) {
-            $data[] = array("#" => "", "Product" => "",  "Quantity" => $item->Reglement, "Total" => $item->sum);
-        }
-        $data[] = $total;
-        return  $data;
+        return DB::select($query, [$from, $to]);
     }
-
-    // public function registerEvents(): array
-    // {
-    //     return [
-    //         AfterSheet::class => function (AfterSheet $event) {
-    //             $cellRange = 'A1:D1'; // All headers
-    //             $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
-
-    //             $styleArray = [
-    //                 'borders' => [
-    //                     'outline' => [
-    //                         'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
-    //                         'color' => ['argb' => 'FFFF0000'],
-    //                     ],
-    //                 ],
-
-    //                 'alignment' => [
-    //                     'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT,
-    //                 ],
-    //             ];
-    //         },
-    //     ];
-    // }
 
     public static function afterSheet(AfterSheet $event)
     {
 
-        $sheet = $event->sheet;
-        $formatting_array = //mpesa
-            [
-                'font' => [
-                    'name' => 'Arial',
-                    'bold' => true,
-                    'italic' => false,
-                    'strikethrough' => false,
-                    'color' => [
-                        'rgb' => '042746'
-                    ]
-                ]
-            ];
-       // $sheet->getStyle('D' . $sheet->getHighestRow() - 1)->applyFromArray($formatting_array);
-        //$sheet->getStyle('D' . $sheet->getHighestRow() - 2)->applyFromArray($formatting_array);
-       // $sheet->getStyle('D' . $sheet->getHighestRow() - 3)->applyFromArray($formatting_array);
-        //$sheet->getStyle('D' . $sheet->getHighestRow())->applyFromArray($formatting_array);
     }
 
     public function headings(): array
     {
-        return ['#', 'Product',  'Quantity', 'Total'];
+        return [ 'Product', 'Department', 'Unit Price',  'Quantity Sold', 'Total'];
     }
 }
